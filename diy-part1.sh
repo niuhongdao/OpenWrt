8fe089b2316f4324aa70446568989d8353d5205f
niuhongdao <125813957+niuhongdao@users.noreply.github.com>
2023-03-11 10:32:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-mkdir -p pakage/helloworld && git clone https://github.com/fw876/helloworld package/helloworld
-#mkdir -p pakage/openwrt-passwall && git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-#mkdir -p pakage/openwrt-passwall/luci-app-passwall && git clone https://github.com/xiaorouji/openwrt-passwall -b luci package/openwrt-passwall/luci-app-passwall
+#mkdir -p pakage/helloworld && git clone https://github.com/fw876/helloworld package/helloworld
+mkdir -p pakage/openwrt-passwall && git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+mkdir -p pakage/openwrt-passwall/luci-app-passwall2 && git clone https://github.com/xiaorouji/openwrt-passwall2 -b luci package/openwrt-passwall/luci-app-passwall2
 
