9f7b0be5f3903fa1a02bf999e80b975707edf2d1
niuhongdao <125813957+niuhongdao@users.noreply.github.com>
2023-07-01 11:24:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.0.253/g' package/base-files/files/bin/config_generate
 
 # Modify errors
-sed -i 's/5.15/5.19/g' /target/linux/rockchip/makefile
+sed -i 's/5.15/5.19/g' target/linux/rockchip/makefile
 
 # Add CPU Temperature
 echo 'msgid "CPU Temperature"'>>feeds/luci/modules/luci-base/po/zh-cn/base.po
