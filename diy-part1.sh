7a94cb02d817d12f95176e152bb7dcc67a7613aa
niuhongdao <125813957+niuhongdao@users.noreply.github.com>
2024-03-12 19:47:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,6 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git clone --depth=1 https://github.com/vernesong/OpenClash package/openclash
 
-sed -i '22,32d' package/openwrt-passwall/gn/Makefile
-rm -rf package/openwrt-passwall/gn/patches
-sed -i '22,32d' package/openwrt-passwall/naiveproxy/Makefile
+# sed -i '22,32d' package/openwrt-passwall/gn/Makefile
+# rm -rf package/openwrt-passwall/gn/patches
+# sed -i '22,32d' package/openwrt-passwall/naiveproxy/Makefile
