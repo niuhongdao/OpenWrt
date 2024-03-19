193c279b7a35b60bb07262a78d717fa2f105abf6
niuhongdao <125813957+niuhongdao@users.noreply.github.com>
2024-03-20 01:41:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.253/g' package/base-files/files/bin/config_generate
 
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/xray-core
+cp -r feeds/passpackages/xray-core feeds/packages/net/
+
 # Add CPU Temperature
 # echo 'msgid "CPU Temperature"'>>feeds/luci/modules/luci-base/po/zh-cn/base.po
 # echo 'msgid "CPU温度"'>>feeds/luci/modules/luci-base/po/zh-cn/base.po
